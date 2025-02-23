4a8dd692367808004ee46ba82216d2b75e86ca5a
Carlos <camoru@gmail.com>
2025-02-23 11:37:27 +0100

    funcionalidad extra: filtro puntos interés.

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mapas Moreno - Hoteles Málaga' });
 });
 
-//ruta de testboostrap, BORRAR LUEGO
+//ruta de testboostrap, solo test instalación
 router.get('/testbootstrap', function(req, res, next) {
   res.render('testbootstrap', { title: 'Prueba de Bootstrap' });
 });
 
-//ruta de testbootstrapsweetalert2, BORRAR LUEGO
+//ruta de testbootstrapsweetalert2, solo test instalación
 router.get('/testbootstrapsweetalert2', function(req, res, next) {
   res.render('testbootstrapsweetalert2', { title: 'Prueba de Bootstrap con Sweetalert2' });
 });
 
-//ruta de testleaflet, BORRAR LUEGO
+//ruta de testleaflet, solo test instalación
 router.get('/testleaflet', function(req, res, next) {
   res.render('testleaflet', { title: 'Prueba de Leaflet' });
 });
